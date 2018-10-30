7f654506e54e6450fcd65b40f241b54f7ac3010a
Christopher Boland <chris.boland97@gmail.com>
2018-10-30 11:27:19 +0000

    Can now filter out results with a distance less than 10km, looking at finding a way for users to set a minimum and maximum distances themselves

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,52 +1,24 @@
 console.log(dataset);
 
 var margin = {top: 20, right: 20, bottom: 50, left: 70},
-    w = 1200 - margin.left - margin.right,
-    h = 600 - margin.top - margin.bottom;
+w = 1200 - margin.left - margin.right,
+h = 600 - margin.top - margin.bottom;
 
 var x = d3.scaleLinear().range([0, w]);
 var y = d3.scaleLinear().range([h, 0]);
 
-var xAxis = x.domain([d3.min(dataset, function(d) {return d.heart_rate; }) - 10, d3.max(dataset, function(d) { return d.heart_rate; })+10]);
-var yAxis = y.domain([d3.max(dataset, function(d) { return d.average_speed; })+0.5, d3.min(dataset, function(d) {return d.average_speed; }) - 0.5]);
+var xAxis = x.domain([d3.min(dataset, function(d) {return d.heart_rate; }) - 5, d3.max(dataset, function(d) { return d.heart_rate; })+5]);
+var yAxis = y.domain([d3.max(dataset, function(d) { return d.average_speed; })+0.2, d3.min(dataset, function(d) {return d.average_speed; }) - 0.2]);
 
-var svgContainer = d3.select('#graph_container').append("g");                                                                   
+var svgContainer = d3.select('#graph_container').append("g");   
+var svg;
 
-//Create SVG variable to build the graph into
-var svg = svgContainer.append('svg')
-    .attr("width", w + margin.left + margin.right)
-    .attr("height", h + margin.top + margin.bottom)
-    .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
-              
-//Append x-axis
-svg.append("g")
-    .attr("transform", "translate(0, "+ h +")")
-    .call(d3.axisBottom(xAxis));
+buildGraph()
+d3.selectAll(".myCheckbox").on("change",update);
+update();
 
-//Append label for x-axis
-svg.append("text")             
-    .attr("transform",
-          "translate(" + (w/2) + " ," + 
-                         (h + margin.top + 20) + ")")
-    .style("text-anchor", "middle")
-    .text("Average Heart Rate (BPM)");
 
-//Append y-axis
-svg.append("g")
-    .call(d3.axisLeft(yAxis));
-
-//Append label for y-axis
-svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x",0 - (h / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("Average Speed (m/s)");  
-
-var dataArray = getDataArray(dataset);
+/**var dataArray = getDataArray(dataset);
 console.log(dataArray)
 var lg = calcLinear(dataArray, "x", "y", d3.min(dataset, function(d){ return d.heart_rate}), d3.min(dataset, function(d){ return d.average_speed}));
 console.log("Scaled values:")
@@ -65,28 +37,7 @@ svg.selectAll("line")
         .attr("y2", y(lg.ptB.y));
 
 //Append data points to the scatterplot
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("a")
-        .attr("xlink:href", function(d) {return "https://www.strava.com/activities/" + d.id})
-        .append("circle")
-            .attr("cx", function(d) {return xAxis(d.heart_rate);})
-            .attr("cy", function(d) {return yAxis(d.average_speed);})
-            .attr("r", 5)
-            .attr("fill", "#ff471a")
-        .on('mouseover', function() {
-            d3.select(this)
-                .transition()
-                .attr("fill", "#000000")
-                .attr("r", 7)
-        })
-        .on('mouseout', function() {
-            d3.select(this)
-                .transition()
-                .attr("fill", "#ff471a")
-                .attr("r", 5)
-        }); 
+
 
 
 function calcLinear(data, x, y, minX, minY){
@@ -173,4 +124,88 @@ function getDataArray(data) {
         dataArray.push(coords)
     })
     return dataArray
+}*/
+
+function update() {
+    console.log("UPDATING")
+    var min_distance = 0;
+    d3.selectAll(".myCheckbox").each(function(d) {
+        cb = d3.select(this);
+        if(cb.property("checked")){
+            min_distance = cb.property("value");
+        }
+    });
+    console.log("MIN DISTANCE: " + min_distance)
+    if(min_distance > 0){
+        console.log("GETTING NEW DATA");
+        newData = dataset.filter(function(d,i) {
+            if (d.distance < min_distance) {
+                return d;
+            }
+        });
+    } else {
+        newData = dataset;     
+    }
+    console.log("NEW DATA SIZE: " + newData.length);
+    console.log("DATA SET SIZE: " + dataset.length);
+
+    svg.selectAll("circle").remove();
+    svg.selectAll("circle")
+        .data(newData)
+        .enter()
+        .append("a")
+            .attr("xlink:href", function(d) {return "https://www.strava.com/activities/" + d.id})
+            .append("circle")
+                .attr("cx", function(d) {return xAxis(d.heart_rate);})
+                .attr("cy", function(d) {return yAxis(d.average_speed);})
+                .attr("r", 5)
+                .attr("fill", "#ff471a")
+            .on('mouseover', function() {
+                d3.select(this)
+                    .transition()
+                    .attr("fill", "#000000")
+                    .attr("r", 7)
+            })
+            .on('mouseout', function() {
+                d3.select(this)
+                    .transition()
+                    .attr("fill", "#ff471a")
+                    .attr("r", 5)
+            }); 
+}
+
+function buildGraph() {                                                               
+    //Create SVG variable to build the graph into
+    svg = svgContainer.append('svg')
+        .attr("width", w + margin.left + margin.right)
+        .attr("height", h + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
+                
+    //Append x-axis
+    svg.append("g")
+        .attr("transform", "translate(0, "+ h +")")
+        .call(d3.axisBottom(xAxis));
+
+    //Append label for x-axis
+    svg.append("text")             
+        .attr("transform",
+            "translate(" + (w/2) + " ," + 
+                            (h + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("Average Heart Rate (BPM)");
+
+    //Append y-axis
+    svg.append("g")
+        .call(d3.axisLeft(yAxis));
+
+    //Append label for y-axis
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (h / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Average Speed (m/s)");  
 }
